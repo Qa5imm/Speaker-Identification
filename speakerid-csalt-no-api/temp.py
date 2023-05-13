3528953b9980de7545d7dc4e0b4ecc7981d36ad3
Qa5im <24100256@lums.edu.pk>
2023-05-13 17:47:32 +0500

    intial

---
+++ speakerid-csalt-no-api/temp.py
@@ -0,0 +1,10 @@
+print("percent calculator")
+
+while (1):
+    total= input("entre total marks\n")
+    obtain= input("enter obtain marks\n")
+    percent= input("enter percent\n")
+    res= (float(obtain)/float(total))*(int(percent))
+    print(res)
+    print("\n")
+
